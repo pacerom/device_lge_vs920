3e1c7a65c69fb50d627bc366db69d7f3bdbd1788
Tom Marshall <tdm.code@gmail.com>
2013-04-15 20:48:12 +0000

    Fix device identifier
    Change-Id: If4879c3b94fa7ce3e12b8c1af3be3c01b12c3121

--- cm.mk
+++ cm.mk
@@ -20,7 +20,7 @@ PRODUCT_RELEASE_NAME := Spectrum
 PRODUCT_VERSION_DEVICE_SPECIFIC :=
 
 ## Device identifier. This must come after all inclusions
-TARGET_PRODUCT := cm_vs920
+PRODUCT_DEVICE := vs920
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=i_vzw TARGET_DEVICE=i_vzw BUILD_FINGERPRINT=Verizon/i_vzw/i_vzw:4.0.4/IMM76D/VS920ZV7.47e5065f:user PRIVATE_BUILD_DESC="i_vzw-user 4.0.4 IMM76D 47e5065f release-keys"
 
