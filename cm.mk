9580b5d19eacb9b765c52afd13f242302846d4cc
Tom Marshall <tdm.code@gmail.com>
2013-10-04 20:45:56 +0000

    vs920: Use new method of picking boot animation
    Change-Id: Ia5be7937c125c4c24a72650c78df984f9b9425c9

--- cm.mk
+++ cm.mk
@@ -5,7 +5,9 @@ PRODUCT_COPY_FILES += \
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/cdma.mk)
 
-TARGET_BOOTANIMATION_NAME := vertical-720x1280
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
